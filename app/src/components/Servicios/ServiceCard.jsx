61e4f6623166071ad46ec6b49f050bb71167fe8e
Rodrigo Mora Carballo <romoraca@est.utn.ac.cr>
2026-08-06 17:45:36 -0600

    Añadida una etiqueta en las cards que indica si está activo o inactivo.

--- app/src/components/Servicios/ServiceCard.jsx
+++ app/src/components/Servicios/ServiceCard.jsx
@@ -6,20 +6,18 @@ import { Link } from "react-router-dom";
 export function ServiceCard({ service }) {
 
     return (
-       <Card className="w-full max-w-sm h-full flex flex-col rounded-[24px] relative group overflow-hidden border-border bg-card text-card-foreground hover:border-primary/50 hover:shadow-xl transition-all duration-300">
+        <Card className="w-full max-w-sm h-full flex flex-col rounded-[24px] relative group overflow-hidden border-border bg-card text-card-foreground hover:border-primary/50 hover:shadow-xl transition-all duration-300">
             <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-transparent via-primary/50 to-transparent opacity-0 group-hover:opacity-100 transition-opacity" />
             <div className="relative h-48 w-full overflow-hidden bg-muted">
-                <Button
-                    variant="outline"
-                    size="icon"
-                    className='absolute top-2 right-2 rounded-full
-                    bg-background/80 backdrop-blur'
-                >
-                    <Heart
-                        className='w-4 h-4 text-red-500'
-                    />
+               
+                    <p
+                        className={`flex w-15 text-center font-bold rounded-full px-3 py-1 text-sm 
+                            ${service.activo ? "bg-green-100 text-green-700" : "bg-red-100 text-red-700" }`}
+                    >
+                        {service.activo ? "Activo" : "Inactivo"}
+                    </p>
 
-                </Button>
+               
             </div>
 
             <CardHeader className="pb-3">
@@ -51,7 +49,9 @@ export function ServiceCard({ service }) {
                 <Button
                     asChild
                     variant="ghost"
-                    className="w-full mt-4 rounded-full border bg-secondary/50 hover:bg-accent hover:text-accent-foreground transition-all duration-300"
+                    className="w-full mt-4 rounded-full border bg-secondary/50 
+                    text-lg py-5
+                    hover:bg-accent hover:text-accent-foreground transition-all duration-300"
                 >
                     <Link
                         to={`/services/${service.id}`}
